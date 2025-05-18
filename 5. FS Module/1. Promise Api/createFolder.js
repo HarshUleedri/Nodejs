fbaa161c7edd8f9b30645118942390fe37f42981
HarshUleedri <ulidari2@gmail.com>
2025-05-18 23:45:50 +0530

    feat: :sparkles: URL module added

--- 5. FS Module/1. Promise Api/createFolder.js
+++ 5. FS Module/1. Promise Api/createFolder.js
@@ -1,7 +1,6 @@
 import * as fs from "fs/promises";
 
-
-//creating Directory / folder 
+//creating Directory / folder
 try {
   await fs.mkdir("c:\\nodejs"); // it create folder only if the previous folder exist
   await fs.mkdir("c:\\nodejs\\course\\tailwindcss", { recursive: true }); // if your not confirm that previous folder does exist or not then use { recursive: true} what does is it just create the folder if folder does not exist in the path
---
+++ 7. URL Module/index.js
@@ -0,0 +1,15 @@
+import { URL } from "url";
+
+const myUrl = new URL("http://www.example.com:8080/p/a/t/h?query=string#hash");
+
+console.log(myUrl.hash);
+console.log(myUrl.host);
+console.log(myUrl.hostname);
+console.log(myUrl.port);
+console.log(myUrl.href);
+console.log(myUrl.protocol);
+console.log(myUrl.search);
+console.log(myUrl.searchParams);
+
+console.log(myUrl.toString);
+console.log(myUrl.toJSON);
